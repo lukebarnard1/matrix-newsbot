4f5852ab31ff2783f28659be2d8c694580892208
lukebarnard <luke.barnard99@gmail.com>
2017-10-06 16:01:46 +0100

    Pull news from NewsAPI

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const sdk = require('matrix-js-sdk');
 const { MatrixClient } = sdk;
-
+const NewsAPI = require('newsapi');
 const request = require('request');
 
 const matrixOptions = {
@@ -24,19 +24,34 @@ try {
 
 const client = new MatrixClient(matrixOptions);
 
-let i = 0;
-const news = [
-	{
-		"title": "Communication in Refugee Camps Solved",
-		"body": "Refugee camps no longer need to rely on external Internet services for internal communications. Matrix.org provides a decentralised alternative to existing messaging systems with the advantage of not being controlled by a single authority. Camp members, aid workers and doctors can collaborate to improve the living conditions through group-chat discussions. News articles, announcements and weather information can now be disseminated to camp members via Riot - the flagship Matrix client - and through the announcement screens placed at information points around the camp.",
-	},
-];
+let newsApi = new NewsAPI(options.newsApiKey);
 
-const sendNews = () => {
-	console.info('Sending news item', i, ':', news[i].title);
-	client.sendStateEvent(options.roomId, options.newsType, news[i], Date.now());
-	i = (i + 1) % news.length;
-	client.sendMessage(options.roomId, {"body": "News sent!", "msgtype": "m.text"});
-};
+newsApi.articles({
+  source: 'bbc-news',
+  sortBy: 'top',
+}).then(articlesResponse => {
+	const news = articlesResponse.articles.map((apiArticle) => {
+		return {
+			title: apiArticle.title,
+			body: apiArticle.description,
+			image: apiArticle.urlToImage,
+			local: false,
+		};
+	});
+	startSendingNews(news);
+});
 
-setInterval(sendNews, options.newsIntervalMs);
+const startSendingNews = (news) => {
+	let i = 0;
+
+	const sendNews = () => {
+		console.info('Sending news item', i, ':', news[i].title);
+		client.sendEvent(options.roomId, options.newsType, news[i]);
+		i = (i + 1) % news.length;
+	};
+
+	sendNews();
+	setInterval(sendNews, options.newsIntervalMs);
+}
+
+
